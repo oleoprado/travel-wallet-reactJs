2515b2ffd3d4f59242639bfee46d826c539a688c
Leonardo Prado <leonardo.rprado@gmail.com>
2022-10-04 14:12:07 +0100

    verificar se passou no req 10

---
+++ src/tests/Table.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import Wallet from '../pages/Wallet';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import mockData from './helpers/mockData';
+
+const initialState = {
+  wallet: {
+    currencies: [
+      'USD',
+      'CAD',
+      'GBP',
+      'ARS',
+      'BTC',
+      'LTC',
+      'EUR',
+      'JPY',
+      'CHF',
+      'AUD',
+      'CNY',
+      'ILS',
+      'ETH',
+      'XRP',
+      'DOGE',
+    ],
+    expenses: [
+      {
+        id: 0,
+        value: '420',
+        description: 'chá da tarde',
+        currency: 'EUR',
+        method: 'Cartão de crédito',
+        tag: 'Alimentação',
+        exchangeRates: mockData,
+      },
+    ],
+    editor: false,
+    idToEdit: 0,
+  },
+};
+
+describe('Testando componente Table', () => {
+  it('verifica se o valor e metodo de pagamento são exibidos na tabela', () => {
+    renderWithRouterAndRedux(<Wallet />, { initialState });
+
+    const valueTable = screen.getByRole('cell', { name: /420\.00/i });
+    const methodTable = screen.getByRole('cell', { name: /cartão de crédito/i });
+
+    expect(valueTable).toBeInTheDocument();
+    expect(methodTable).toBeInTheDocument();
+  });
+
+  it('verifica se a label do botão(add despesa) é alterada ao clicar no botao de editar', () => {
+    renderWithRouterAndRedux(<Wallet />, { initialState });
+
+    const addExpenseBtn = screen.getByRole('button', { name: /adicionar despesa/i });
+    expect(addExpenseBtn).toBeInTheDocument();
+
+    const editBtn = screen.getByRole('button', { name: /editar/i });
+    expect(editBtn).toBeInTheDocument();
+  });
+
+  it('verifica se o botao EXCLUIR está com o comportamento esperado', () => {
+    renderWithRouterAndRedux(<Wallet />, { initialState });
+
+    const deleteBtn = screen.getByRole('button', { name: /excluir/i });
+    expect(deleteBtn).toBeInTheDocument();
+    userEvent.click(deleteBtn);
+    expect(deleteBtn).not.toBeInTheDocument();
+  });
+});
